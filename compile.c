074db004324e4017072ef35fc6fde20fa27ad590
kichikuou <KichikuouChrome@gmail.com>
2020-03-20 17:12:21 +0900

    Emit SR command of old format in System3.5 mode

--- compile.c
+++ compile.c
@@ -919,7 +919,7 @@ static bool command(void) {
 	case CMD2('S', 'O'): arguments("v"); break;
 	case CMD2('S', 'P'): arguments("ee"); break;
 	case CMD2('S', 'Q'): arguments("eee"); break;
-	case CMD2('S', 'R'): arguments("nv"); break;
+	case CMD2('S', 'R'): arguments(sys_ver == SYSTEM35 ? "ev" : "nv"); break;
 	case CMD2('S', 'S'): arguments("e"); break;
 	case CMD2('S', 'T'): arguments("e"); break;
 	case CMD2('S', 'U'): arguments("vv"); break;
