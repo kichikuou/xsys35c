1c86f0363e495871532f7532c50faf8339a7f877
kichikuou <KichikuouChrome@gmail.com>
2020-04-19 22:59:04 +0900

    Add rtt.sh (round trip testing) script
    This decompiles given ALD file, compiles the generated source code,
    and verifies it matches the original ALD.

---
+++ rtt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# rtt.sh: Round trip testing
+#
+# This decompiles given ALD file, compiles the generated source code,
+# and verifies it matches the original ALD.
+#
+# Usage: rtt.sh aldfile [ainfile]
+#
+set -e
+
+rm -rf rtt_adv
+mkdir rtt_adv
+./decompiler/xsys35dc -o rtt_adv $1 $2
+./compiler/xsys35c -p rtt_adv/xsys35c.cfg -o rtt_adv/out.ald -a rtt_adv/out.ain
+./tools/ald compare $1 rtt_adv/out.ald
+if [ -n "$2" ]; then
+	cmp $2 rtt_adv/out.ain
+fi
