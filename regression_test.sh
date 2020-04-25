3eda1926b84f71308602e701f41a6aa3ec33b1e1
kichikuou <KichikuouChrome@gmail.com>
2020-04-25 10:20:29 +0900

    Add regression test for control structures

--- regression_test.sh
+++ regression_test.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+# To update testdata/regression_test.ald, remove it and run this script
 set -e
 
 compiler/xsys35c --timestamp 0 -p testdata/source/xsys35c.cfg -o testdata/actual.ald
-cmp testdata/regression_test.ald testdata/actual.ald
+if [ -f testdata/regression_test.ald ]; then
+    ./tools/ald compare testdata/regression_test.ald testdata/actual.ald
+else
+    cp testdata/actual.ald testdata/regression_test.ald
+fi
 rm -rf testdata/decompiled
 mkdir -p testdata/decompiled
 decompiler/xsys35dc -o testdata/decompiled testdata/actual.ald
