62e469d214df95f0d2231d88bbf81171a666293d
kichikuou <KichikuouChrome@gmail.com>
2020-06-13 09:39:35 +0900

    rtt.sh: Add -o option specifying output directory
    If it's not specified, a temporary directory is used.
    This makes it easy to run tests in parallel.

--- rtt.sh
+++ rtt.sh
@@ -5,14 +5,32 @@
 # This decompiles given ALD file, compiles the generated source code,
 # and verifies it matches the original ALD.
 #
-# Usage: rtt.sh aldfile [ainfile]
-#
-set -e
 
-rm -rf rtt_adv
-./decompiler/xsys35dc -a -o rtt_adv $1 $2
-./compiler/xsys35c -p rtt_adv/xsys35c.cfg -o rtt_adv/out.ald -a rtt_adv/out.ain
-./tools/ald compare $1 rtt_adv/out.ald
-if [ -n "$2" ]; then
-	cmp $2 rtt_adv/out.ain
+if [ $# -eq 0 ]; then
+	echo 'Usage: rtt.sh [options] <aldfile> [<ainfile>]'
+	echo 'Options:'
+	echo '    -o <directory>  Generate outputs into <directory>'
+	exit 1
 fi
+
+if [ "$1" = "-o" ]; then
+	out="$2"
+	shift 2
+	cleanup=
+else
+	out=$(mktemp -d)
+	cleanup="rm -rf $out"
+fi
+
+Exit() {
+	$cleanup
+	exit $1
+}
+
+./decompiler/xsys35dc -a -o "$out" "$@" || Exit 1
+./compiler/xsys35c -p "$out"/xsys35c.cfg -o "$out"/out.ald -a "$out"/out.ain || Exit 1
+./tools/ald compare "$1" "$out"/out.ald || Exit 1
+if [ -n "$2" ]; then
+	cmp "$2" "$out"/out.ain || Exit 1
+fi
+Exit 0
