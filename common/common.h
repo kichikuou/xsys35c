b63b588162921dfb23efc6f774001b40108d1b19
kichikuou <KichikuouChrome@gmail.com>
2020-07-05 22:31:32 +0900

    Version 1.0.0

--- common/common.h
+++ common/common.h
@@ -21,7 +21,7 @@
 #include <stdnoreturn.h>
 #include <time.h>
 
-#define VERSION "0.1.0"
+#define VERSION "1.0.0"
 
 static inline uint32_t le32(const uint8_t *p) {
 	return p[0] | p[1] << 8 | p[2] << 16 | p[3] << 24;
