kichikuou/xsys35c

commit 588fca22f42181614f6ac8437375413462ab4b59

Ensure input buffer ends with a newline
This prevents bugs like #8.

compiler/xsys35c.c
@@ -89,11 +89,12 @@ static char *read_file(const char *path) {
 		error("%s: %s", path, strerror(errno));
 	if (fseek(fp, 0, SEEK_SET) != 0)
 		error("%s: %s", path, strerror(errno));
-	char *buf = malloc(size + 1);
+	char *buf = malloc(size + 2);
 	if (size > 0 && fread(buf, size, 1, fp) != 1)
 		error("%s: read error", path);
 	fclose(fp);
-	buf[size] = '\0';
+	buf[size] = '\n';
+	buf[size + 1] = '\0';
 
 	if (config.utf8) {
 		const char *err = validate_utf8(buf);

compiler/compile_test.c
@@ -97,9 +97,6 @@ void compile_test(void) {
 	TEST("block-comment",
 		 "R/*A*A\nA*/R",
 		 "RR");
-	TEST("cmnt@lastline",
-		 "R ; no newline",
-		 "R");
 
 	TEST("data array",
 		 "[1, 0b100000000, 0xffff]",

compiler/lexer.c
@@ -67,13 +67,8 @@ void skip_whitespaces(void) {
 		} else if (isspace(*input)) {
 			input++;
 		} else if (*input == ';' || (*input == '/' && *(input+1) == '/')) {
-			char *newline = strchr(input, '\n');
-			if (!newline) {
-				// No newline at end of file.
-				input = strchr(input, '\0');
-			} else {
-				input = newline;
-			}
+			// This is safe because the input is guaranteed to end with "\n\0".
+			input = strchr(input, '\n');
 		} else if (*input == '/' && *(input+1) == '*') {
 			const char* top = input;
 			do {

commit 32d38cfb59e47cb228cff36b779047b220592f36

Ignore empty lines at the end of variables.txt
This fixes a regression bug introduced by 588fca2, where xsys35c emitted
an extra empty item in the VARI section of System39.ain.

compiler/xsys35c.c
@@ -126,6 +126,9 @@ static Vector *read_var_list(const char *path) {
 	char *line;
 	while ((line = next_line(&buf)) != NULL)
 		vec_push(vars, trim_right(line));
+	// Drop empty lines at the end
+	while (vars->len > 0 && !((char *)vars->data[vars->len - 1])[0])
+		vars->len--;
 	return vars;
 }
 